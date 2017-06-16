2a601b9dd538b5b16f3e0ddc460fe75cb5d58c37
Kevin Zhan <kevin.w.zhan@gmail.com>
2017-06-16 00:25:31 -0400

    Update Server

--- startall.bat
+++ startall.bat
@@ -1,3 +1,3 @@
 start ngrok tcp 25565
 
-start java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.11.2-13.20.0.2228-universal.jar nogui
+start java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.11.2-13.20.0.2315-universal.jar nogui
