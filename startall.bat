59ebf9381213a68c2a9d80446954113dda8131f9
Kevin Zhan <kevin.w.zhan@gmail.com>
2017-08-11 20:19:21 -0400

    1.12.1 update

--- startall.bat
+++ startall.bat
@@ -1,3 +1,3 @@
 start ngrok start minecraft ssh
 
-start java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.11.2-13.20.0.2366-universal.jar nogui
+start java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.12.1-14.22.0.2452-universal.jar nogui
