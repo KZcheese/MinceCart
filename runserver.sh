0f870607dee6521fade944661621a0527493cdf7
Kevin Zhan <kevin.w.zhan@gmail.com>
2017-08-19 00:31:02 -0400

    Update Forge

--- runserver.sh
+++ runserver.sh
@@ -1,1 +1,1 @@
-java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.12.1-14.22.0.2452-universal.jar nogui
+java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.12.1-14.22.0.2459-universal.jar nogui
