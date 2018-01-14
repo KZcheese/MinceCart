c7b03b14727b97412d0e45d3306df0058625bf94
Kevin Zhan <kevin.w.zhan@gmail.com>
2018-01-14 14:56:25 -0500

    end of holiday update

--- startall.bat
+++
@@ -1,3 +0,0 @@
-start ngrok tcp 25565
-
-start java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.12.2-14.23.1.2568-universal.jar nogui
---
+++ startserver.bat
@@ -0,0 +1,1 @@
+java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.12.2-14.23.1.2589-universal.jar nogui
